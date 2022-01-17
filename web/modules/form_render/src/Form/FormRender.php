625f78087b120f9884cb47d448f8f0c41f6e15eb
Andrii Kondratenko <andrii.kondratenko@smile-ukraine.com>
2022-01-17 16:09:07 +0200

    Trainig to create form config

--- web/modules/form_render/src/Form/FormRender.php
+++ web/modules/form_render/src/Form/FormRender.php
@@ -4,6 +4,7 @@ namespace Drupal\form_render\Form;
 
 use Drupal\Core\Form\FormBase;
 use Drupal\Core\Form\FormStateInterface;
+use Drupal\Core\Messenger\Messenger;
 
 /**
  * Implements form_reder controller.
@@ -24,6 +25,14 @@ class FormRender extends FormBase {
       '#default_value' => $default_value,
     ];
 
+    // Загружаем настройки модули из формы CollectPhoneSettings.
+    $config = \Drupal::config('form_render.settings');
+    $form['phone'] = [
+      '#type' => 'tel',
+      '#title' => $this->t('Your phone number'),
+      '#default_value' => $config->get('phone')
+    ];
+
     $form['submit'] = [
       '#type' => 'submit',
       '#value' => 'Поиск',
@@ -41,7 +50,8 @@ class FormRender extends FormBase {
   }
 
   public function submitForm(array &$form, FormStateInterface $form_state) {
-
+    // Выводим имя введенное в форме.
+    \Drupal::messenger()->addMessage($this->t('Your name is @name', array('@name' => $form_state->getValue('name'))));
   }
 
 }
---
+++ web/modules/form_render/src/Form/FormRenderSettings.php
@@ -0,0 +1,62 @@
+<?php
+
+namespace Drupal\form_render\Form;
+
+use Drupal\Core\Form\ConfigFormBase;
+use Drupal\Core\Form\FormStateInterface;
+use Drupal\Core\Messenger\Messenger;
+
+/**
+ * Implements form_render controller for config.
+ */
+class FormRenderSettings extends ConfigFormBase {
+
+  /**
+   * {@inheritdoc}
+   */
+  public function getFormId() {
+    return 'form_render_settings';
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  protected function getEditableConfigNames() {
+    // Возвращает названия конфиг файла.
+    // Значения будут храниться в файле:
+    // form_render.settings.yml
+    return [
+      'form_render.settings',
+    ];
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function buildForm(array $form, FormStateInterface $form_state) {
+    // Загружаем наши конфиги.
+    $config = $this->config('form_render.settings');
+    // Добавляем поле для возможности задать телефон по умолчанию.
+    // Далее мы будем использовать это значение в предыдущей форме.
+    $form['default_phone_number'] = [
+      '#type' => 'textfield',
+      '#title' => $this->t('Default phone number'),
+      '#default_value' => $config->get('phone'),
+    ];
+    // Субмит наследуем от ConfigFormBase
+    return parent::buildForm($form, $form_state);
+
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function submitForm(array &$form, FormStateInterface $form_state) {
+    $values = $form_state->getValues();
+    // Записываем значения в наш конфиг файл и сохраняем.
+    $this->config('form_render.settings')
+      ->set('phone', $values['default_phone_number'])
+      ->save();
+  }
+
+}
